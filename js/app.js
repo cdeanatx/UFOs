d205c85465c8e7012422fb620570a54076ccf888
Curtis Dean <cdean.atx@gmail.com>
2021-10-20 15:06:16 -0500

    created function to loop through array and convert to HTML table

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@ const tableData = data;
 // reference the HTML table using D3
 var tbody = d3.select("tbody");
 
+// build an HTML table out of the array from data.js
+function buildTables(data) {
+    // clear any existing data from the table
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+        // create a row in the HTML file for each entry in the array
+        let row = tbody.append("tr");
+        
+        // add mutiple "cells" to the current table row
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
