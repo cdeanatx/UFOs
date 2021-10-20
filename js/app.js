db474d0396f5bdabe355f711815adb2a2cdb0ecf
Curtis Dean <cdean.atx@gmail.com>
2021-10-20 16:00:28 -0500

    created function to loop through array and convert to HTML table

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 // build an HTML table out of the array from data.js
-function buildTables(data) {
+function buildTable(data) {
     // clear any existing data from the table
     tbody.html("");
 
@@ -20,3 +20,21 @@ function buildTables(data) {
         });
     });
 }
+
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    buildTable(filteredData);
+
+    
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
